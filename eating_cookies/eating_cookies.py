55bf90a7cd46c56a183e0fe56244c666dac2e0a0
Christine Smith <christinecsmith1@gmail.com>
2020-10-06 20:52:44 -0700

    mvp for first pass tests

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,14 +1,34 @@
 '''
 Input: an integer
 Returns: an integer
+
+if n = 1:
+    return 1 
+
+elif n = 2: 
+    return 2
+
+else n >= 3: 
+
+    example:    50 =>  f(49) f(48) + f(47)...
 '''
 def eating_cookies(n):
     # Your code here
-
-    pass
+    counter = 0
+    if n == 0: 
+        return 1
+    elif n == 1: 
+        return 1
+    elif n == 2: 
+        return 2
+    elif n == 3: 
+        return 4
+    else:
+        counter += 1
+        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
